e83f22e68b5eb135f4cf441d6995a2519729693a
Chris Senior <christopher.senior@gmail.com>
2012-04-18 21:43:23 +0100

    Created basic plugin framework for builder and nature

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Tycho helper plugins
 These plugins are intended to make it easier to work with Tycho based builds. There are several basic functionalities implemented:
 
 * A tycho project nature to distinguish tycho based projects from other Maven projects
-* An Eclipse builder to validate that the key POM and manifest attributes are in sync (mark as error in POM if not)
+* An Eclipse builder to validate that the key POM and manifest attributes are in sync (mark as error in POM/MANIFEST if not)
     * POM `artifactId` = Manifest `Bundle-SymbolicName`
     * POM `version` = Manifest `Bundle-Version`
     * POM `version` = `*-SNAPSHOT` then Manifest `Bundle-Version` = `*.qualifier`
     * (optional) POM `name` = Manifest `Bundle-Name`
-* Quick fix for above errors to sync from Manifest
+* Quick fix for above errors to sync from/to Manifest
 * Project/Workspace preference page(s) to control ERROR/WARNING level for above markers
 * A project context menu item to "enable tycho" on a PDE project
     * Add the Maven project nature etc.
@@ -25,4 +25,6 @@ License
 -------
 
 These plugins are licensed under the [Eclipse Public License v1.0](http://www.eclipse.org/legal/epl-v10.html).
+
+As you might expect these plugins use Maven/Tycho for their build.
 
---
+++ com.duckasteroid.tycho-helper.core/src/com/duckasteroid/tycho/utils/core/builder/TychoBuilder.java
@@ -0,0 +1,61 @@
+package com.duckasteroid.tycho.utils.core.builder;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/**
+ * This builder loads PDE and maven models and checks they are in sync: 
+ * <ul>
+ * <li>POM `artifactId` = Manifest `Bundle-SymbolicName`</li> 
+ * <li>POM `version` = Manifest`Bundle-Version` </li>
+ * <li>POM `version` = `*-SNAPSHOT` then Manifest `Bundle-Version` = `*.qualifier`</li> 
+ * <li>(optional) POM `name` = Manifest `Bundle-Name`</li>
+ * </ul> 
+ * @author chris
+ */
+public class TychoBuilder extends IncrementalProjectBuilder {
+
+	@Override
+	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
+		if (shouldAudit(kind)) {
+			doAudit(monitor);
+		}
+		return null;
+	}
+
+	/**
+	 * Check the build type and the resource delta to determine if an audit is required
+	 * @param kind The build kind
+	 * @return true if audit required
+	 */
+	private boolean shouldAudit(int kind) {
+		if (kind == FULL_BUILD)
+			return true;
+		IResourceDelta delta = getDelta(getProject());
+		if (delta == null)
+			return false;
+		IResourceDelta[] children = delta.getAffectedChildren();
+		for (int i = 0; i < children.length; i++) {
+			IResourceDelta child = children[i];
+			String fileName = child.getProjectRelativePath().lastSegment();
+			if (fileName.equals("pom.xml") || fileName.equals("MANIFEST.MF"))
+				return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Actually perform the audit
+	 * @param monitor A progress monitor
+	 */
+	private void doAudit(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
